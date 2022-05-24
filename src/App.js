d9625da8da913b30cb2d53b4eb69e1d95f029acf
Alexandr <caiiiia_podoplelov@mail.ru>
2022-05-24 14:11:56 +0300

    basic states

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [ likes, setLikes ] = useState(0);
+  const [ inputValue, setInputValue] = useState('Some text');
+
+  const incr = () => setLikes(likes + 1);
+  const decr = () => setLikes(likes - 1);
+
   return (
     <div className="App">
-      
+      <h1>{likes}</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}/>
+      <button onClick={incr}>plus one</button>
+      <button onClick={decr}>minus one</button>
     </div>
   );
 }
