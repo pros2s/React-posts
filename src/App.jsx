901a90575cd2fcd5535ca19d779917cb70ca1682
Alexandr <caiiiia_podoplelov@mail.ru>
2022-05-27 14:18:52 +0300

    self hook usePosts

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 
 import PostFilter from './components/PostFilter';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import PostModal from './components/UI/modal/PostModal';
 
+import { usePosts } from './hooks/usePosts';
+
 import AddNewButton from './components/UI/button/AddNewButton';
 
 
 function App() {
-  const [ posts, setPosts ] = useState([
-    { id: 1, title: 'b', description: 'a 1' },
-    { id: 2, title: 'a', description: 'b 2' },
-    { id: 3, title: 'c', description: 'c 3' }
-  ]);
+  const [ posts, setPosts ] = useState([]);
   const [ filter, setFilter ] = useState({ search: '', selectedSort: '' });
   const [ showModal, setShowModal ] = useState(false);
-
-
-  const sortedPosts = useMemo(() => {
-    return filter.selectedSort
-      ? [ ...posts ].sort((a, b) => a[filter.selectedSort].localeCompare(b[filter.selectedSort]))
-      : posts
-  }, [ filter.selectedSort, posts ]);
-
-  const filteredPosts = useMemo(() => {
-    return sortedPosts.filter((post) => post.title.toLowerCase().includes(filter.search));
-  }, [ filter.search, sortedPosts ])
+  const filteredPosts = usePosts(posts, filter.selectedSort, filter.search);
 
 
   const createPost = (newPost) => {
---
+++ src/hooks/usePosts.js
@@ -0,0 +1,21 @@
+import { useMemo } from "react";
+
+
+export const useSortedPosts = (posts, selectedSort) => {
+  const sortedPosts = useMemo(() => {
+    return selectedSort
+      ? [ ...posts ].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+      : posts
+  }, [ selectedSort, posts ]);
+
+  return sortedPosts;
+};
+
+export const usePosts = (posts, selectedSort, search) => {
+  const sortedPosts = useSortedPosts(posts, selectedSort);
+  const filteredPosts = useMemo(() => {
+    return sortedPosts.filter((post) => post.title.toLowerCase().includes(search));
+  }, [ search, sortedPosts ]);
+
+  return filteredPosts;
+};
