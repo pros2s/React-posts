b7b1a1b1f716ddb327b4d3a53b219463f75eaa73
Alexandr <caiiiia_podoplelov@mail.ru>
2022-05-27 13:13:24 +0300

    animation of posts

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 import PostItem from './PostItem';
 
 import './styles.css';
+import './postAnimation.css';
 
 
 const PostList = ({ posts, remove }) => {
   if (!posts.length) {
-    return <h1>Post list is empty! :{'('}</h1>;
+    return (
+      <h1>Post list is empty! :{'('}</h1>
+    );
   };
 
   return (
     <div>
       <div className='post__list'>
-        { posts.map((post, i) => <PostItem remove={ remove } number={ i + 1 } post={ post } key={ post.id }/>) }
+        <TransitionGroup>
+          {
+            posts.map((post, i) => (
+              <CSSTransition
+                key={ post.id }
+                timeout={ 450 }
+                classNames='post__animation'
+              >
+                <PostItem remove={ remove } number={ i + 1 } post={ post }/>
+              </CSSTransition>
+            ))
+          }
+        </TransitionGroup>
       </div>
     </div>
   );
