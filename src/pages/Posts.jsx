02a5b95f7e2d72e353174c3abf01891dcf9984a5
Alexandr <caiiiia_podoplelov@mail.ru>
2022-05-29 17:59:20 +0300

    infinite loading posts with IntersectionObserver

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import PostFilter from '../components/PostFilter';
 import PostList from '../components/PostList';
@@ -9,6 +9,7 @@ import PostError from '../components/UI/errorer/PostError';
 import PostPagination from '../components/UI/pagination/PostPagination';
 
 import { usePosts } from '../hooks/usePosts';
+import { useObserver } from '../hooks/useObserver';
 import useFetch from '../hooks/useFetch';
 
 import AddNewButton from '../components/UI/button/AddNewButton';
@@ -18,6 +19,7 @@ import { getTotalPages } from '../utils/getTotalPages';
 import '../styles/footer.scss';
 
 const Posts = () => {
+  //hooks
   const [ posts, setPosts ] = useState([]);
   const [ filter, setFilter ] = useState({ search: '', selectedSort: '' });
   const [ showModal, setShowModal ] = useState(false);
@@ -27,20 +29,25 @@ const Posts = () => {
   const [ limit ] = useState(10);
   const [ page, setPage ] = useState(1);
 
+  const loadRef = useRef();
+
   const filteredPosts = usePosts(posts, filter.selectedSort, filter.search);
 
-  useEffect(() => { fetchData() }, [ page ]);// eslint-disable-line
-
   const [ fetchData, isLoading, isError ] = useFetch(async () => {
-    const posts = await PostService.getAll(limit, page);
-    setPosts(posts.data);
+    const response = await PostService.getAll(limit, page);
+    setPosts([ ...posts, ...response.data ]);
 
-    const total = showModal ? totalCount : posts.headers['x-total-count'];
+    const total = showModal ? totalCount : response.headers['x-total-count'];
     setTotalCount(total);
     setTotalPages(getTotalPages(total, limit));
   });
 
+  useEffect(() => { fetchData() }, [ page ]);// eslint-disable-line
 
+  useObserver(loadRef, isLoading, (page < totalPages) , () => { setPage(page + 1); });
+
+
+  //functions
   const createPost = (newPost, total) => {
     setPosts([ ...posts, newPost ]);
     setShowModal(false);
@@ -57,22 +64,32 @@ const Posts = () => {
   };
 
 
+  //render
   return (
     <>
-      <PostFilter filter={ filter } setFilter={ setFilter }/>
       { isError && <PostError/> }
-      { isLoading
-        ? <PostLoader/>
-        : <PostList err={ isError } remove={ removePost } posts={ filteredPosts }/>
-      }
+      { isLoading && <PostLoader/> }
+
+      <PostFilter filter={ filter } setFilter={ setFilter }/>
+      <PostList err={ isError } remove={ removePost } posts={ filteredPosts }/>
 
       <PostModal showModal={ showModal } setShowModal={ setShowModal }>
         <PostForm total={ totalCount } create={ createPost }/>
       </PostModal>
 
+      <div className="loadMore" ref={ loadRef }/>
+
       <div className='footer'>
-        <AddNewButton disabled={ isError } onClick={ () => setShowModal(true) }>Add new post</AddNewButton>
-        <PostPagination page={ page } totalPages={ totalPages } currentPage={ currentPage }/>
+        <AddNewButton
+          disabled={ isError }
+          onClick={ () => setShowModal(true) }>
+            Add new post
+        </AddNewButton>
+
+        <PostPagination
+          page={ page }
+          totalPages={ totalPages }
+          currentPage={ currentPage }/>
       </div>
     </>
   );
